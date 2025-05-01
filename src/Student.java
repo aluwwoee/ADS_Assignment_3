de8618497613523e4a32bb694455936f5470f940
aluwwoe <banichban186@gmail.com>
2025-05-01 23:25:12 +0500

    added Student class

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private String fullName;
+
+    public Student(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + fullName + '}';
+    }
+}
